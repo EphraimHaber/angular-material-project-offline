97cafc9c84096ea72c748f53ee2d09df8570d9d0
EphraimHaber <ephraimxyz@gmail.com>
2024-11-28 13:15:02 +0200

    prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,7 @@ import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [
-    RouterOutlet,
-    MatSlideToggleModule,
-    MatButtonModule,
-    MatCardModule,
-    MatIconModule,
-  ],
+  imports: [RouterOutlet, MatSlideToggleModule, MatButtonModule, MatCardModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
