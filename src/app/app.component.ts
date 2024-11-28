08e685561466e60c1f915d4d719cfac0db9321e3
EphraimHaber <ephraimxyz@gmail.com>
2024-11-28 13:09:43 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-root',
+  imports: [
+    RouterOutlet,
+    MatSlideToggleModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'supsup';
+}
